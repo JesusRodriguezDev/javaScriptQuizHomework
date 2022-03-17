e19071b625a2cfdeb29857cdb633bd2673bbcf03
Jesus Rodriguez <jesusrodriguez@Jesuss-MacBook-Pro.local>
2022-03-16 21:07:26 -0600

    added questions variable to script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer: "alerts",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+    {
+        question:"The condition in an if/else statement is enclosed within ____",
+        answer:"parentheses",
+        choices:[
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brakets"
+        ]
+    },
+    {
+        question:"Arrays in JavaScritp can be used to store ____.",
+        answer:"all of the above",
+        choices:[
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ]
+    },
+    {
+        question:"String values must be closed within ____ when being assigned to variables.",
+        answer:"quotes",
+        choices:[
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parentheses"
+        ] 
+    },
+    {
+        question:"A very useful too used during development and debugging for printing content to the debugger is:",
+        answer:"console.log",
+        choices:[
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        ] 
+    }
+]
